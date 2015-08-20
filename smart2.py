c0e2730bb9ca26579086367685385328a2b7065c
vivian-xu <vivian.zxx53@gmail.com>
2015-08-20 17:56:37 +0800

    id,姓名，贡献值统计_完成

--- smart2.py
+++ smart2.py
@@ -5,8 +5,9 @@ import re
 BASE_DIR = __file__[:-9]
 cache_dir = BASE_DIR + 'cache'
 item_ptn = re.compile(ur'class="user-info">(.*?)</p>',re.DOTALL)
-info_ptn = re.compile(ur'href="/member/(\d*)">(.*?)</a>.*?title="(.*?)">',re.DOTALL)
-#info_ptn = re.compile(ur'href="/member/(\d*)">(.*?)</a>.*?title="(.*?)">.*?class=(.*?)>',re.DOTALL)
+info_ptn = re.compile(ur'href="/member/(\d*)">(.*?)</a>.*?(\d*-\d*)?">',re.DOTALL)
+#info_ptn = re.compile(ur'href="/member/(\d*)">(.*?)</a>.*?title="(.*?)">',re.DOTALL)
+
 
 for f in os.listdir(cache_dir): #一个店铺主页
     filename = cache_dir+'/'+f
@@ -16,6 +17,7 @@ for f in os.listdir(cache_dir): #一个店铺主页
     with open(filename) as fp:
         content = ' '.join(fp.readlines()).decode('utf8')
     item_ret = item_ptn.findall(content)  # 一个店铺每个评价的id和分数信息
+#    print ' '.join(item_ret)
 
     for item in item_ret:  # item是每一个评价的信息 ，item_ret 是一个店铺中所有评价
         info_ret = info_ptn.findall(item)
