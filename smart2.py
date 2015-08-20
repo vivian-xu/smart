87b1ccabe6106e0f3036dfbe6d5b71e3cda27f1a
vivian-xu <vivian.zxx53@gmail.com>
2015-08-20 17:31:54 +0800

    id,姓名，贡献值统计_未完
    已基本完成解析工作

---
+++ smart2.py
@@ -0,0 +1,25 @@
+# -*- Encoding: utf-8 -*-
+import os
+import re
+
+BASE_DIR = __file__[:-9]
+cache_dir = BASE_DIR + 'cache'
+item_ptn = re.compile(ur'class="user-info">(.*?)</p>',re.DOTALL)
+info_ptn = re.compile(ur'href="/member/(\d*)">(.*?)</a>.*?title="(.*?)">',re.DOTALL)
+#info_ptn = re.compile(ur'href="/member/(\d*)">(.*?)</a>.*?title="(.*?)">.*?class=(.*?)>',re.DOTALL)
+
+for f in os.listdir(cache_dir): #一个店铺主页
+    filename = cache_dir+'/'+f
+#    print filename
+    content = ''
+
+    with open(filename) as fp:
+        content = ' '.join(fp.readlines()).decode('utf8')
+    item_ret = item_ptn.findall(content)  # 一个店铺每个评价的id和分数信息
+
+    for item in item_ret:  # item是每一个评价的信息 ，item_ret 是一个店铺中所有评价
+        info_ret = info_ptn.findall(item)
+        for id,nm,sc in info_ret:
+            print id,nm,sc
+#        for id,nm,sc,vi in info_ret:
+#            print id,nm,sc,vi
